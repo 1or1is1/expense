541ec4b18f675cbe3b9d37f6408f63a029557329
Ankit <kankit476@gmail.com>
2023-10-06 21:11:36 +0530

    Updated Navbar changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Auth, user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  private auth = inject(Auth);
+  user$ = user(this.auth);
+}
